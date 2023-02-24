092dcf9ca8df96ad52d8995dfee4db32fba67069
seyed0123 <seyed123ali123@gmail.com>
2023-02-24 21:13:47 +0330

    GUI (swing) added little update for report

--- src/main/java/WeatherApp.java
+++ src/main/java/WeatherApp.java
@@ -2,26 +2,32 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import org.json.JSONObject;
 
+import org.json.JSONObject;
+import javax.swing.*;
 import java.util.Objects;
 import java.util.Scanner;
+
 public class WeatherApp{
     // Copy your API-KEY here
     public final static String apiKey = "4170999efb19400b9e921713232302";
     public static void main(String[] args)
     {
+        //swing();
+
         Scanner scanner = new Scanner(System.in);
-        String city  = scanner.nextLine();
+        //String city  = scanner.nextLine();
+        String city = JOptionPane.showInputDialog("Enter the city name");
         String JsonWetter;
         while(true)
         {
             JsonWetter= getWeatherData(city);
             if(JsonWetter==null)
             {
-                System.out.print("some thing went wrong. The city isn't found or the Internet isn't connected.please check them.\nif you want to exit enter \"exit\" or enter a city name to continue.\n");
-                city=scanner.nextLine();
-                if(Objects.equals(city, "exit"))
+                //System.out.print("some thing went wrong. The city isn't found or the Internet isn't connected.please check them.\nif you want to exit enter \"exit\" or enter a city name to continue.\n");
+                JOptionPane.showMessageDialog(null,"some thing went wrong. The city isn't found or the Internet isn't connected.please check them.","ERROR",JOptionPane.ERROR_MESSAGE);
+                city = JOptionPane.showInputDialog("Enter the city name");
+                if(Objects.equals(city, null))
                     return;
             }else
                 break;
@@ -33,7 +39,8 @@ public class WeatherApp{
         double windSpeed = getWindSpeed(jsonObject);
         String windDirect = getWindDirect(jsonObject);
         double feelTemp = getFeelTemp(jsonObject);
-        outputTerminal(city, temp , humy ,windDegree ,windSpeed , windDirect ,feelTemp);
+        city=getSity(jsonObject);
+        output(city, temp , humy ,windDegree ,windSpeed , windDirect ,feelTemp);
     }
 
 
@@ -55,11 +62,16 @@ public class WeatherApp{
                 stringBuilder.append(line);
             }
             reader.close();
+            connection.disconnect();
             return stringBuilder.toString();
         } catch (Exception e) {
             //e.printStackTrace();
             return null;
         }
+    }
+    public static String getSity(JSONObject jsonObject)
+    {
+        return jsonObject.getJSONObject("location").getString("name")+"."+jsonObject.getJSONObject("location").getString("region")+"."+jsonObject.getJSONObject("location").getString("country");
     }
     /**
      * get temperature for a specific city.
@@ -118,48 +130,59 @@ public class WeatherApp{
 
     /**
      * print input with 15 character width.
-     * @param str
+     * @param str string do you want to print
      */
-    public static void setw(String str)
+    public static String setw(String str, int size)
     {
-        if(str.length()<=15)
+        if(str.length()<=size)
         {
-            while(str.length()<=15)
+            while(str.length()<=size)
             {
                 str+=" ";
             }
         }
-        System.out.print(str);
+        return str;
     }
 
-    public static void setw(int input)
+    public static String setw(int input , int size)
     {
         String str = Integer.toString(input);
-        if(str.length()<=15)
+        if(str.length()<=size)
         {
-            while(str.length()<=15)
+            while(str.length()<=size)
             {
                 str+=" ";
             }
         }
-        System.out.print(str);
+        return str;
     }
-    public static void setw(double input)
+    public static String setw(double input, int size)
     {
         String str = Double.toString(input);
-        if(str.length()<=15)
+        if(str.length()<=size)
         {
-            while(str.length()<=15)
+            while(str.length()<=size)
             {
                 str+=" ";
             }
         }
-        System.out.print(str);
+        return str;
     }
 
-    public static void outputTerminal(String city, double temp , int humy , int windDegree , double windSpeed , String windDirect , double feelTemp)
+    public static void output(String city, double temp , int humy , int windDegree , double windSpeed , String windDirect , double feelTemp)
     {
-        setw("city");setw("temperature");setw("humidity");setw("wind degree");setw("wind speed(khp)");setw("wind direct");setw("fesls-like temp");
-        System.out.println("");setw(city);setw(temp);setw(humy);setw(windDegree);setw(windSpeed+"(khp)");setw(windDirect);setw(feelTemp);
+        String out;
+        //out=setw("city", city.length()+3)+setw("temperature", 12 )+setw("humidity", 10)+setw("wind degree", 12)+setw("wind speed(khp)",15)+setw("wind direct" , 12)+setw("fesls-like temp", 15);
+        //out+="\n"+setw(city,city.length()+3)+setw(temp,12)+setw(humy,10)+setw(windDegree,12)+setw(windSpeed+"(khp)" , 15)+setw(windDirect,12)+setw(feelTemp,15);
+        out="city : "+city;
+        out+="\ntemperature : "+ temp;
+        out+="\nhumidity : "+ humy;
+        out+="\nwind degree : " + windDegree;
+        out+="\nwind Speed (kph) : " + windSpeed ;
+        out+="\nwind direction : " + windDirect;
+        out+="\nfeels-like temperature : " + feelTemp;
+        JOptionPane.showMessageDialog(null,out);
+        //System.out.print(out);
     }
+
 }
