3e4d5c81a41d9f712c2ca734307730c3e2983cdb
mobina kargar <dymamsijhidjj@gmail.com>
2023-03-01 12:52:21 +0330

    End of making

--- readme.md
+++ readme.md
@@ -5,7 +5,7 @@ I built a simple Java application that connects to a weather API and retrieves i
 ##  Summary
 I used Swing for It's GUI and It outPuts the result in the JFrame . It contains Error message frame and Action event .
 I complete the main function by input the city and I offer  2 options:
-1) If the city dose not exist or the net is too poor to check the weather of the city that you are looking for, it will outputs Error
+1) If the city does not exist or the net is too poor to check the weather of the city that you are looking for, it will outputs Error
 2) In next part I called JASON object to figure out the humid then I put the key:Current in it. And with the advantage of JASON's get string I found the next key: humid.
 And I did the same for other weather conditions.
 At the time of printing I put the result in it's own specific function.
@@ -14,7 +14,7 @@ And the job of these function are to return string to Integer or double. (For pr
 ## Features
    **Examples :**
 
-Correct City --->[[Screenshot-2023-03-01-at-10-32-45.png](https://i.postimg.cc/Rhyhrj0n/Screenshot-2023-03-01-at-10-32-45.png)](https://postimg.cc/XBK4d1dV)[[Screenshot-2023-03-01-at-10-33-21.png](https://i.postimg.cc/cHWf2tzN/Screenshot-2023-03-01-at-10-33-21.png)](https://postimg.cc/8FyFrcDy)
+known City --->[[Screenshot-2023-03-01-at-10-32-45.png](https://i.postimg.cc/Rhyhrj0n/Screenshot-2023-03-01-at-10-32-45.png)](https://postimg.cc/XBK4d1dV)[[Screenshot-2023-03-01-at-10-33-21.png](https://i.postimg.cc/cHWf2tzN/Screenshot-2023-03-01-at-10-33-21.png)](https://postimg.cc/8FyFrcDy)
 
 Unknown City --->
 [[Screenshot-2023-03-01-at-10-50-32.png](https://i.postimg.cc/90gVRSwF/Screenshot-2023-03-01-at-10-50-32.png)](https://postimg.cc/gLh11gKf)
--- src/main/java/WeatherApp.java
+++ src/main/java/WeatherApp.java
@@ -1,15 +1,95 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import org.json.JSONObject;
-import java.util.Scanner;
-
+import javax.swing.*;   //  I Called it to use simple GUI
 public class WeatherApp {
-    // Copy your API-KEY here
-    public final static String apiKey = "API-KEY";
-    // TODO: Write main function
-    public static void main(String[] args) {
+    static JFrame pic;
+    public final static String apiKey = "253fe6e8f2bd47cf97745159232502";   //  <--- Copy of API_KEY
+    public static void main(String[] args)  {
+        //In this section I make label and Frame to use this app in file instead of in terminal--->
+        pic = new JFrame("City's Weather");
+        pic.setVisible(true);
+        pic.setSize(1000, 100);
+        pic.setLayout(new FlowLayout());
+        JLabel l1 = new JLabel("Which Location That You Look For ?");
+        pic.add(l1);
+        JTextField Location = new JTextField(20);
+        pic.add(Location);
+        JButton e = new JButton("Click");
+        pic.add(e);
+
+        //This thing below says that when you click on it, it closes the page --->
+        e.addActionListener((ActionEvent e1) -> {
+
+            String s = e1.getActionCommand();
+
+            if (s.equals("Click")) {
+
+                pic.setVisible(false);
+                // open new page --->
+                pic = new JFrame("City's Weather Result");
+                pic.setVisible(true);
+                pic.setSize(900, 100);
+                pic.setLayout(new FlowLayout());
+                String city = Location.getText();
+
+                //I make Error message for the null answer--->
+                if (getWeatherData(city) == null) {
+
+                    pic.setVisible(false);
+                    JFrame frame = new JFrame("City's Weather Result");
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    JOptionPane.showMessageDialog(frame ,"Sorry! The City does not Found or your Net is Weak!",
+                            "ERROR", JOptionPane.ERROR_MESSAGE);
+
+                }
+
+                else {
+
+                    JSONObject WeatherJson = new JSONObject(getWeatherData(city));
+
+                    //in this part I try to Show the weather's condition( Use it by JSON) --->
+                    String Humid = Integer.toString(WeatherJson.getJSONObject("current").getInt("humidity"));
+                    String Celsius_Temperature = Double.toString(WeatherJson.getJSONObject("current").getDouble("temp_c"));
+                    String Wind_Speed = Double.toString(WeatherJson.getJSONObject("current").getDouble("wind_kph"));
+                    String Wind_Direction = WeatherJson.getJSONObject("current").getString("wind_dir");
+
+                    //put the weathers condition to the page --->
+                    JLabel l112 = new JLabel("Humid : ");
+                    pic.add(l112);
+                    JLabel l2 = new JLabel(String.valueOf(getHumidity(Humid)));
+                    pic.add(l2);
+                    JLabel l3 = new JLabel("%      ");
+                    pic.add(l3);
+
+                    JLabel l4 = new JLabel("Celsius Temperature : ");
+                    pic.add(l4);
+                    JLabel l5 = new JLabel(String.valueOf(getTemperature(Celsius_Temperature)));
+                    pic.add(l5);
+                    JLabel l6 = new JLabel(" C       ");
+                    pic.add(l6);
+
+                    JLabel l7 = new JLabel("Wind Speed : ");
+                    pic.add(l7);
+                    JLabel l8 = new JLabel(String.valueOf(getTemperature(Wind_Speed)));
+                    pic.add(l8);
+                    JLabel l9 = new JLabel(" m/s      ");
+                    pic.add(l9);
+
+                    JLabel l10 = new JLabel("Wind Direction : ");
+                    pic.add(l10);
+                    JLabel l11 = new JLabel(Wind_Direction);
+                    pic.add(l11);
+
+                }
+
+            }
+
+        });
 
     }
 
@@ -19,34 +99,52 @@ public class WeatherApp {
      * @param city the name of the city for which weather data should be retrieved
      * @return a string representation of the weather data, or null if an error occurred
      */
-    public static String getWeatherData(String city) {
+    public static String getWeatherData (String city){
+
         try {
+
             URL url = new URL("http://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + city);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             StringBuilder stringBuilder = new StringBuilder();
             String line;
+
             while ((line = reader.readLine()) != null) {
+
                 stringBuilder.append(line);
+
             }
+
             reader.close();
             return stringBuilder.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+
         }
+
+        // In this part if you delete the  "e.printStackTrace();" you can takeout this Error --->
+        catch (Exception e) {
+
+            return null;
+
+        }
+
     }
 
-    // TODO: Write getTemperature function returns celsius temperature of city by given json string
-    public static double getTemperature(String weatherJson){
+    //By this Function we can change String to Double --->
+    public static double getTemperature (String weatherJson){
+
         double answer = 0.0;
+        answer = Double.parseDouble(weatherJson);
         return answer;
+
     }
 
-    // TODO: Write getHumidity function returns humidity percentage of city by given json string
-    public static int getHumidity(String weatherJson){
+    //By this Function we can change String to Int --->
+    public static int getHumidity (String weatherJson){
+
         int answer = 0;
+        answer = Integer.parseInt(weatherJson);
         return answer;
+
     }
 }
